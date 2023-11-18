7a3f7d507fce590ec67e441001d952f7fc331aa3
とうしみ <tech@toushimi.jp>
2023-11-19 02:49:15 +0900

    Change: embed用のイベントメソッドとtwitter用ライブラリのrequireをする(kikisenbot-4 Twitter URLのEmbedを展開する)

--- bot/event.rb
+++ bot/event.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 require_relative 'tts'
-
+require_relative 'lib/twitter_url'
 # @param [Discordrb::Bot] bot
 # @param [Discordrb::Channel] chan
 # on boot/connect channel notification
@@ -16,3 +16,18 @@ def bot_join_voice_channel(event)
   end
 end
 
+# @param [Discordrb::Events::MessageEvent] event
+def embedded_twitter_url(event)
+  m = /(https:\/\/x\.com\/.+)|(https:\/\/twitter\.com\/.+) *\Z/.match(event.message.content)
+  return unless m
+  url = m[1]
+  twitter_status = TwitterURL.new(event.message.content)
+  event.channel.send_embed do |embed|
+    embed.color = 0xfaab00
+    embed.author = twitter_status.author
+    embed.thumbnail = twitter_status.thumbnail
+    embed.image = twitter_status.image
+    embed.title = twitter_status.description
+    embed.url = twitter_status.article
+  end
+end
