f777a871c5f933331a1b39ac149bad262c565bcd
とうしみ <tech@toushimi.jp>
2023-11-19 02:48:26 +0900

    Change: Twitterの用ライブラリを更新(kikisenbot-4 Twitter URLのEmbedを展開する)
    基本的にはやりたくないが、APIからだと有料でかなーーーり高い。Ferrum+Chromiumでスクレイピングする。

--- bot/lib/twitter_url.rb
+++ bot/lib/twitter_url.rb
@@ -1,20 +1,69 @@
 # frozen_string_literal: true
 
 class TwitterURL
+  FERRUM_OPTION={headless: :new, window_size: [1280,720], browser_options: {'no-sandbox': nil}}
+  USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+  OGTITLE_REGEXP=/(?<author>.+)on\sX:\s\"(?<description>.+)\"/m
+  attr_reader :author, :icon, :description, :image, :article, :thumbnail
+  def initialize(url)
+    @browser = Ferrum::Browser.new(FERRUM_OPTION)
+    @twitter_url = url
+    @browser.headers.set('User-Agent': USER_AGENT)
+    @browser.go_to(@twitter_url)
+    @browser.network.wait_for_idle
+    fetch
+    close
+  end
 
-  # class variables
-  @@twitter = false
+  private
+  def fetch
+    @author ||= _author
+    @description ||= _description
+    @thumbnail ||= _thumbnail
+    @image ||= _image
+    @article ||= _article
+  end
 
-  def initialize
-    def twitter_client_authentication
-      @@twitter ||= Twitter::REST::Client.new do |config|
-        config.consumer_key    = ENV.fetch("TWITTER_CONSUMER_KEY")
-        config.consumer_secret = ENV.fetch("TWITTER_CONSUMER_SECRET")
-      end
-    end
+  def close
+    @browser.quit
+  end
 
-    def self.status(id)
-      @@twitter.status(id)
-    end
+  def _author
+    author_name = _author_name
+    author_icon = _icon
+    author_url  = _author_url
+    Discordrb::Webhooks::EmbedAuthor.new(name: author_name,url: author_url.to_s,icon_url: author_icon.to_s)
+  end
+
+  def _author_name
+    tmp = @browser.at_css("meta[property='og:title']").attribute 'content'
+    m = OGTITLE_REGEXP.match(tmp)
+    m[:author]
+  end
+  def _icon
+    URI.parse(@browser.at_css('article img[src*=profile_images]').attribute 'src')
+  end
+
+  def _description
+    tmp = @browser.at_css("meta[property='og:title']").attribute 'content'
+    m = OGTITLE_REGEXP.match(tmp)
+    m[:description]
+  end
+
+  def _thumbnail
+    Discordrb::Webhooks::EmbedThumbnail.new(url: _icon)
+  end
+  def _image
+    Discordrb::Webhooks::EmbedImage.new(url: URI.parse(@browser.at_css("article img[src*='media']").attribute 'src'))
+  end
+
+  def _article
+    URI.parse(@twitter_url)
+  end
+
+  def _author_url
+    author_url = URI.parse(@twitter_url)
+    author_url.path = File.dirname(File.dirname(author_url.path))
+    author_url
   end
 end
